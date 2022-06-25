cd25e4d9c91bd1c960d9b7a3d36a1ab9327f75c8
nooromari <nooromari847@gmail.com>
2022-06-25 22:29:28 +0300

    create repoCard Component

--- src/components/repoCard/repoCard.component.jsx
+++ src/components/repoCard/repoCard.component.jsx
@@ -1,9 +1,72 @@
+import ListItem from "../listItem/listItem.component";
+import "./repoCard.styles.css";
 
+const RepoCard = ({
+  repoData: {
+    full_name,
+    created_at,
+    updated_at,
+    language,
+    forks_count,
+    open_issues_count,
+    stargazers_count,
+    avatar_url,
+    html_url,
+    license_type,
+  },
+  setSavedRepos,
+}) => {
 
-const RepoCard = (props) => (
+    const listData = [
+        {
+            title: "Stars",
+            value: stargazers_count,
+            icon:""
+        },
+        {
+            title: "Forks",
+            value: forks_count,
+            icon:""
+        },
+        {
+            title: "Open Issues",
+            value: open_issues_count,
+            icon:""
+        },
+        {
+            title: "Age",
+            value: created_at,
+            icon:""
+        },
+        {
+            title: "Last Commit",
+            value: updated_at,
+            icon:""
+        },
+        {
+            title: "License",
+            value: license_type,
+            icon:""
+        },
+        {
+            title: "Language",
+            value: language,
+            icon:""
+        },
+    ]
+  return (
     <div>
-        <p>Repo name</p>
+      <a href={html_url} target="_blank" rel="noreferrer">
+        <img src={avatar_url} alt={full_name} />
+        <h3>{full_name}</h3>
+      </a>
+      <dl>
+        {listData.map(({value, title})=> 
+            <ListItem key={full_name+value} value={value} title={title} />    
+        )}
+      </dl>
     </div>
-);
+  );
+};
 
 export default RepoCard;
